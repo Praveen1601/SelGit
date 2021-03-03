fcbc3649e4222937184de95d6216d688b2c8d2bb
Praveen1601 <praveenvskumar55@gmail.com>
2021-03-03 18:18:56 +0530

    SELENIUM

---
+++ src/org/test/SelDay1.java
@@ -0,0 +1,17 @@
+package org.test;
+
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.chrome.ChromeDriver;
+
+public class SelDay1 {
+	public static void main(String[] args) {
+		System.setProperty("webdriver.chrome.driver","C:\\Users\\GOD\\eclipse-workspace\\SelProj\\driver\\chromedriver.exe");
+		WebDriver driver = new ChromeDriver();
+		driver.get(" https://www.facebook.com/");
+		WebElement sel = driver.findElement(By.id("email"));
+		sel.sendKeys("praveen");
+	}
+
+}
